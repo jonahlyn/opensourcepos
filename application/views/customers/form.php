2a97fc784812e7f9b80382bb9d16fb53b4f32ca7
Patrick <xenth@xenth.com>
2017-09-27 22:59:43 -0600

    Added form_date field for html5 browsers

--- application/views/customers/form.php
+++ application/views/customers/form.php
@@ -52,7 +52,7 @@
 				<div class="form-group form-group-sm">
 					<?php echo form_label($this->lang->line('customers_company_name'), 'company_name', array('class' => 'control-label col-xs-3')); ?>
 					<div class='col-xs-8'>
-						<?php echo form_input(array(
+						<?php echo form_date(array(
 								'name'=>'company_name',
 								'id'=>'company_name',
 								'class'=>'form-control input-sm',
